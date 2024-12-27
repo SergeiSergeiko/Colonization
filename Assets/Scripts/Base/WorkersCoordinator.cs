7c31d3bdf10d6bead7e10d51b941686a906241b2
s.sergei <s.sergeiko.blr@gmail.com>
2024-12-27 18:43:28 +0100

    accepts workers from "Base" and works with them

--- Assets/Scripts/Base/WorkersCoordinator.cs
+++ Assets/Scripts/Base/WorkersCoordinator.cs
@@ -6,12 +6,23 @@ public class WorkersCoordinator : MonoBehaviour
 {
     [SerializeField] private Base _base;
 
+    private List<Worker> _workers = new();
+    private List<Worker> _freeWorkers = new();
+    private List<Worker> _busyWorkers = new();
     private List<Transform> _foundResources = new();
     private Coroutine _working;
 
-    public void StartWorking(List<Transform> resources)
+    private void OnDisable()
     {
-        _foundResources = resources;
+        _busyWorkers.ForEach(worker => worker.Freed -= OnWorkerFreed);
+
+        if (_working != null)
+            StopCoroutine(_working);
+    }
+
+    public void StartWorking(List<Transform> resources, List<Worker> workers)
+    {
+        InitWorkersCoordinator(resources, workers);
 
         if (_working != null)
             StopCoroutine(_working);
@@ -21,21 +32,59 @@ public class WorkersCoordinator : MonoBehaviour
 
     private IEnumerator Working()
     {
-        WaitUntil waitFreeWorker = new(() => _base.CountFreeWorkers > 0);
+        WaitUntil waitFreeWorker = new(() => _freeWorkers.Count > 0);
 
         while (_foundResources.Count > 0)
         {
-            if (_base.TryGetFreeWorker(out Worker worker))
+            if (_freeWorkers.Count > 0)
             {
-                Transform resource = _foundResources[0];
-                worker.ExtractResource(resource);
-                _foundResources.Remove(resource);
+                SendWorker(_freeWorkers[0], _foundResources[0]);
 
                 yield return null;
             }
             else
             {
                 yield return waitFreeWorker;
+            }
+        }
+    }
+
+    private void SendWorker(Worker worker, Transform resource)
+    {
+        worker.ExtractResource(resource);
+        worker.Freed += OnWorkerFreed;
+
+        _busyWorkers.Add(worker);
+        _freeWorkers.Remove(worker);
+        _foundResources.Remove(resource);
+    }
+
+    private void OnWorkerFreed(Worker worker)
+    {
+        worker.Freed -= OnWorkerFreed;
+
+        _freeWorkers.Add(worker);
+        _busyWorkers.Remove(worker);
+    }
+
+    private void InitWorkersCoordinator(List<Transform> resources, List<Worker> workers)
+    {
+        _workers = workers;
+        _foundResources = resources;
+        _freeWorkers.Clear();
+        _busyWorkers.Clear();
+
+        foreach (Worker worker in workers)
+        {
+            if (worker.IsBusy)
+            {
+                worker.Freed += OnWorkerFreed;
+
+                _busyWorkers.Add(worker);
+            }
+            else
+            {
+                _freeWorkers.Add(worker);
             }
         }
     }
