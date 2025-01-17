1a7fce82f1888261afd3cf1ebff012d4abdb85c8
s.sergei <s.sergeiko.blr@gmail.com>
2025-01-18 00:42:51 +0100

    Add build system

---
+++ Assets/Scripts/Builder.cs
@@ -0,0 +1,82 @@
+using System;
+using UnityEngine;
+
+public class Builder : MonoBehaviour
+{
+    private Building _building;
+    private Camera _camera;
+    private bool _isMovingBuilding;
+    private Vector3 _positionBuilding;
+
+    public event Action<Building> BuildingPlaced;
+
+    private void Awake()
+    {
+        _camera = Camera.main;
+    }
+
+    private void Update()
+    {
+        if (_building != null)
+            PlaceBuilding();
+    }
+
+    public Building Build(Building building, Vector3 position)
+    {
+        return Instantiate(building, position, Quaternion.identity);
+    }
+
+    public void StartPlacingNewBuilding(Building prefab)
+    {
+        CancelPlacing();
+
+        _isMovingBuilding = false;
+        _building = Instantiate(prefab);
+    }
+
+    public void StartReplacingBuilding(Building building)
+    {
+        CancelPlacing();
+
+        _isMovingBuilding = true;
+        _building = building;
+        _positionBuilding = _building.transform.position;
+    }
+
+    private void PlaceBuilding()
+    {
+        var ray = _camera.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit))
+        {
+            _building.transform.position = hit.point;
+
+            if (hit.transform.TryGetComponent(out Plane _))
+            {
+                if (Input.GetMouseButtonDown(0))
+                {
+                    BuildingPlaced?.Invoke(_building);
+                    _building = null;
+                }
+            }
+        }
+    }
+
+    private void CancelPlacing()
+    {
+        if (_building != null)
+        {
+            if (_isMovingBuilding)
+            {
+                _building.transform.position = _positionBuilding;
+                BuildingPlaced.Invoke(_building);
+            }
+            else
+            {
+                Destroy(_building.gameObject);
+                BuildingPlaced.Invoke(null);
+            }
+        }
+    }
+}
