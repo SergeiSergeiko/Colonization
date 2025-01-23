e73dbe9013d19ac95c816072449df48d984e55f2
s.sergei <s.sergeiko.blr@gmail.com>
2025-01-23 01:26:42 +0100

    code review

--- Assets/Scripts/Effects/Effect.cs
+++ Assets/Scripts/Effects/Effect.cs
@@ -1,12 +1,24 @@
 using System;
 using UnityEngine;
 
+[RequireComponent(typeof(ParticleSystem))]
 public class Effect : MonoBehaviour
 {
+    private ParticleSystem _particleSystem;
+
     public event Action<Effect> Stoped;
 
     private void OnParticleSystemStopped()
     {
         Stoped?.Invoke(this);
+    }
+
+    public void SetSizeScannerEffect(float size)
+    {
+        if (_particleSystem == null)
+            _particleSystem = GetComponent<ParticleSystem>();
+
+        ParticleSystem.MainModule main = _particleSystem.main;
+        main.startSize = size;
     }
 }
